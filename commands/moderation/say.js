a5a10fe6f1d3f9a1726ae9e09e064f2e917f4ab8
daan <daanschenkel@ziggo.nl>
2020-03-13 19:50:56 +0100

    Update say.js

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -16,12 +16,8 @@ module.exports = {
 
         const roleColor = message.guild.me.highestRole.hexColor;
 
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
-
-            message.channel.send(embed);
+  
+            message.channel.send(args.join(" "));
         } else {
             message.channel.send(args.join(" "));
         }
