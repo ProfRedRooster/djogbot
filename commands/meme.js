8c3a45f839ee1adc2a1f63650d86659548da1021
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 11:56:52 +0100

    Update

--- commands/meme.js
+++ commands/meme.js
@@ -1,25 +1,16 @@
-const Discord = require("discord.js");
+const discord = require("discord.js");
+const randomPuppy = require("random-puppy");
 
 module.exports.run = async(bot, message, args) => {
+    const subReddits = ["meme", "memes"];
+    // Grab a random property from the array
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
 
-    try {
-        const { body } = await snekfetch
-            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
-            .query({ limit: 800 });
-        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        if (!allowed.length) return sg.channel.send('It seems we are out of fresh memes!, Try again later.');
-        const randomnumber = Math.floor(Math.random() * allowed.length)
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .setTitle(allowed[randomnumber].data.title)
-        .setDescription("Posted by: " + allowed[randomnumber].data.author)
-        .setImage(allowed[randomnumber].data.url)
-        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        .setFooter("Memes provided by r/dankmemes")
-        msg.channel.send(embed)
-    } catch (err) {
-        return console.log(err);
-    }
+    // Get a random image from the subreddit page
+    const img = await randomPuppy(random);
+
+
+    message.channel.send(img);
 }
 
 module.exports.help = {
