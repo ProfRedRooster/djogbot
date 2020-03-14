118f420955ef6b36df559046118dae0dfd2a955c
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 15:34:48 +0100

    Update

--- commands/groningen.js
+++ commands/groningen.js
@@ -1,8 +1,12 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-
-
+// Check if they have one of many roles
+if(message.member.roles.some(r=>["Groningen", "Appingedam"].includes(r.name)) ) {
+    // has one of the roles
+    return message.channel.send("Je hebt al een locatie geselecteerd!");
+  } else {
+    // has none of the roles
     let role = message.guild.roles.find(r => r.name === "Groningen");
 
     // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
@@ -13,6 +17,9 @@ module.exports.run = async(bot, message, args) => {
     // Add the role!
     member.addRole(role).catch(console.error);
     message.channel.send("Welkom bij het Groningen Team " + message.member.name + "!");
+  }
+
+    
 
 
             
