bb5be94d4e295bf10adcce717f5f614bcc7a0926
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 09:29:02 +0100

    Update

--- commands/les.js
+++ commands/les.js
@@ -1,4 +1,4 @@
-const discord = require("discord.js");
+const Discord = require("discord.js");
 const rm = require('discord.js-reaction-menu');
 
 module.exports.run = async(bot, message, args) => {
