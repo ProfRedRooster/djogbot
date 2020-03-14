25f16f2bc419976c73186b0865ec4f64f8d33492
daan <daanschenkel@ziggo.nl>
2020-03-14 16:04:07 +0100

    poging 4

--- commands/watmoetikdoen.js
+++ commands/watmoetikdoen.js
@@ -2,15 +2,14 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-
+ message.delete();
     message.channel.send("Goede vraag! Het antwoord is simpel: doen wat Richel zegt :sunglasses:");
     setTimeout(function(){ 
-        message.delete();
+       
         message.channel.send("Okee, nu serieus ... :sunglasses:");
         message.channel.send(" ");
         message.channel.send("We gaan aan de slag met de hoofdstukken in een van de drie vakken die Richel geeft! \n\n Gebruik de volgende commandos \n\n");
         setTimeout(function(){ 
-            message.delete();
             message.channel.send("!arduino om met Arduino te beginnen");
             message.channel.send("!mbot om met mBot te beginnen");
             message.channel.send("!processing om met Processing te beginnen");
