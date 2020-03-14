96be5a2c207dba1893585583a9c71450c03f3fed
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-14 17:23:43 +0100

    Misschien als een regel?

--- commands/watmoetikdoen.js
+++ commands/watmoetikdoen.js
@@ -6,16 +6,10 @@ module.exports.run = async(bot, message, args) => {
     message.channel.send("Goede vraag! Het antwoord is simpel: doen wat Richel zegt :sunglasses:");
     setTimeout(function(){ 
         message.delete();
-        message.channel.send("Okee, nu serieus ... :sunglasses:");
-        message.channel.send(" ");
-        message.channel.send("We gaan aan de slag met de hoofdstukken in een van de drie vakken die Richel geeft!");
+        message.channel.send("Okee, nu serieus ... :sunglasses:\n \nWe gaan aan de slag met de hoofdstukken in een van de drie vakken die Richel geeft!");
         setTimeout(function(){ 
             message.delete();
-            message.channel.send("Gebruik een van de volgende commando's:");
-            message.channel.send("\n \n");
-            message.channel.send("!arduino om met Arduino te beginnen");
-            message.channel.send("!mbot om met mBot te beginnen");
-            message.channel.send("!processing om met Processing te beginnen");
+            message.channel.send("Gebruik een van de volgende commando's: \n \n !arduino om met Arduino te beginnen \n !mbot om met mBot te beginnen \n !processing om met Processing te beginnen");
             return;
          }, 1000); //time in milliseconds
      }, 1000); //time in milliseconds
