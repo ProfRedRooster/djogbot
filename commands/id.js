20e886813ff54a5323c7c16679b9ad61cf288362
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 09:21:57 +0100

    Update

--- commands/id.js
+++ commands/id.js
@@ -2,8 +2,8 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-   return message.author.send("De server id is:" + message.guild.id);
-    
+   message.author.send("De server id van de server" + message.guild.name + "is: " + message.guild.id);
+    message.channel.send("Ik heb de server id naar je gestuurd!");
 }
 
 module.exports.help = {
