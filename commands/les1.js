7359a7a2a7698d6a7be5f898144d41c863b89bfa
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 10:13:41 +0100

    Update

--- commands/les1.js
+++ commands/les1.js
@@ -4,11 +4,14 @@ module.exports.run = async(bot, message, args) => {
   
     message.channel.send("Welkom bij de les: Hoe geef ik aan wie ik ben?");
     setTimeout(function(){ 
+        message.delete();
         message.channel.send("Laten we beginnen!");
         setTimeout(function(){ 
+            message.delete();
             message.channel.send("Als eerste, ben je van groningen of appingedam? doe ?rank [Groningen of Appingedam] ik geef je een halve minuut voordat we verdergaan");
             setTimeout(function(){ 
                 //Code
+                message.delete();
                 message.channel.send("\n \n Oke! de tijd is om!");
                 
                 message.channel.send("\n \nje hebt de les gehaald! doe hem opnieuw met !les1");
