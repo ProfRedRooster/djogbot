e76aa51aebf129ea2100fa895015111b21c6eccb
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 09:46:39 +0100

    Update

--- commands/les.js
+++ commands/les.js
@@ -2,9 +2,7 @@ const Discord = require("discord.js");
 const rm = require('discord.js-reaction-menu');
 
 module.exports.run = async(bot, message, args) => {
-    new rm.menu(message.channel, message.author.id, [message.channel.send("test")], message.channel.send("test2")
-    new Discord.RichEmbed({title:'test3'}), new Discord.RichEmbed({title:'test4'}), new Discord.RichEmbed({title:'test5'})
-
+  
 }
 
 module.exports.help = {
