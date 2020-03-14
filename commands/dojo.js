cb16f5ae8f89e9361c7063e6091e9d33d44a9c60
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 14:32:09 +0100

    Update

---
+++ commands/dojo.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+  return message.channel.send("Hier vind je meer informatie over de  DOJO: https://is.gd/qt7uiD")
+    
+}
+
+module.exports.help = {
+
+    name: "dojo"
+
+}
