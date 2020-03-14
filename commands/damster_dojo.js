2c964ca8cfb1f15afd9bf5d3f294df3785e9bf93
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 14:42:04 +0100

    Update

---
+++ commands/damster_dojo.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+  return message.channel.send("Ga naar de volgende link Voor meer informatie over Damster DOJO: https://is.gd/qOAveq")
+    
+}
+
+module.exports.help = {
+
+    name: "damster_dojo"
+
+}
