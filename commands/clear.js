9ed65c8bc52f10c425a027d3f80cf9898602a38d
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 12:11:38 +0100

    Update

--- commands/clear.js
+++ commands/clear.js
@@ -2,7 +2,6 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 msg = message;
-message.delete();
 const amount = args.join(' '); // Amount of messages which should be deleted
 
 if (!amount) return msg.reply('Hoeveel berichten moet ik verwijderen?'); // Checks if the `amount` parameter is given
@@ -10,7 +9,7 @@ if (isNaN(amount)) return msg.reply('Ben ik nou dom of is dat geen nummer'); // 
 
 if (amount > 100) return msg.reply('Oh, te veel werk, heb ik geen zin in, ik ga alleen akkoord met 100 berichten max (ligt aan discord api)'); // Checks if the `amount` integer is bigger than 100
 if (amount < 1) return msg.reply('Uhh, dat is onmogelijk'); // Checks if the `amount` integer is smaller than 1
-
+message.delete();
 await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
     msg.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
 )});
