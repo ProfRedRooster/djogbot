80a4cde2690fb3d81df2af9ec490252906b64809
Daan Schenkel <daan@Mac-Pro-van-Eric.local>
2020-03-14 11:50:06 +0100

    Update

--- commands/meme.js
+++ commands/meme.js
@@ -2,23 +2,24 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    const embed = new Discord.RichEmbed();
-    got('https://www.reddit.com/r/memes/random/.json').then(response => {
-        let content = JSON.parse(response.body);
-        let permalink = content[0].data.children[0].data.permalink;
-        let memeUrl = `https://reddit.com${permalink}`;
-        let memeImage = content[0].data.children[0].data.url;
-        let memeTitle = content[0].data.children[0].data.title;
-        let memeUpvotes = content[0].data.children[0].data.ups;
-        let memeDownvotes = content[0].data.children[0].data.downs;
-        let memeNumComments = content[0].data.children[0].data.num_comments;
-        embed.addField(`${memeTitle}`, `[View thread](${memeUrl})`);
-        embed.setImage(memeImage);
-        embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`);
+    try {
+        const { body } = await snekfetch
+            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return sg.channel.send('It seems we are out of fresh memes!, Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const embed = new Discord.RichEmbed()
+        .setColor(0x00A2E8)
+        .setTitle(allowed[randomnumber].data.title)
+        .setDescription("Posted by: " + allowed[randomnumber].data.author)
+        .setImage(allowed[randomnumber].data.url)
+        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+        .setFooter("Memes provided by r/dankmemes")
         msg.channel.send(embed)
-            .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
-        console.log('Bot responded with: ' + memeImage);
-    }).catch(console.error);
+    } catch (err) {
+        return console.log(err);
+    }
 }
 
 module.exports.help = {
