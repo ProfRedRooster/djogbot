36f24ef0fbb205f965c0015237beaa93bc516891
Daan Schenkel <daanschenkel@ziggo.nl>
2020-03-16 18:58:10 +0100

    Super fancy ranja

--- commands/ranja.js
+++ commands/ranja.js
@@ -2,9 +2,14 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-
-	
-	message.channel.send("https://thumbs.dreamstime.com/b/glas-rote-limonade-39906916.jpg");
+const embed = {
+  "title": "Geniet van je DigiRanja",
+  "color": 16209920,
+  "image": {
+    "url": "https://thumbs.dreamstime.com/b/glas-rote-limonade-39906916.jpg"
+  }
+};
+message.channel.send({ embed });
 }
 
 module.exports.help = {
