ad319137999022a7dece92dbe71dc2e61af12c68
Daan Schenkel <daanschenkel@ziggo.nl>
2020-03-16 18:44:39 +0100

    Ranja

---
+++ commands/ranja.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+  return message.channel.send("Hier is je DigiRanja! https://thumbs.dreamstime.com/b/glas-rote-limonade-39906916.jpg")
+    
+}
+
+module.exports.help = {
+
+    name: "ranja"
+
+}
