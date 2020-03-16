10a2bf9523e39280d0e954bf03eab17d508a436e
Daan <you@example.com>
2020-03-16 16:05:23 +0100

    Groningen poging 3

--- commands/groningen.js
+++ commands/groningen.js
@@ -1,6 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
+    const guildMember = message.member;
 
 guildMember.addRole('688081051259502787');  
 return message.channel.send("Welkom in het Groningen team!");
