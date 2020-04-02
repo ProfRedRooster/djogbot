b79a3cd16e103a04765f8abbe3d239aa997b0b40
daan <daanschenkel@ziggo.nl>
2020-04-02 19:23:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,13 +51,14 @@ const activities_list = [
     "Leest iemand dit echt?",
     "Pizza is lekker!",
     "Mijn aanrader voor eten? een dubbele Accuburger met extra stroom!",
-    "BAGUETTE!"
+    "BAGUETTE!",
+    "Yeet"
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 5000); // Runs this every 10 seconds.
+    }, 5000); // Runs this every 5 seconds.
 
 
 
