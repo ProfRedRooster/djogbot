5b79729f20b2a55c34f460dc9afc8224efc4bf02
daan <daanschenkel@ziggo.nl>
2020-04-02 18:43:19 +0200

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -2,16 +2,18 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-  message.channel.send("Misbruik van dit commando is strafbaar!")
   // Turn bot off (destroy), then turn it back on
 function resetBot(channel) {
     // send channel a message that you're resetting bot [optional]
-    message.channel.send('Restarting...')
+    message.channel.send('Opnieuw Opstarten...')
     .then(msg => client.destroy())
     .then(() => client.login(process.env.token))
     .then (() => message.channel.send("Klaar!"))
     
 }
+   resetBot(message.channel);
+            break;
+
     
 }
 
