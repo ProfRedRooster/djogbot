0b871d65e71f2a6c2f70938a80c2218d41e9938f
daan <daanschenkel@ziggo.nl>
2020-04-09 19:53:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const botConfig = require("./botConfig.json");
 const fs = require("fs");
 const bot = new discord.Client();
 bot.commands = new discord.Collection();
-const profanity = require('./swearwords.json');
 
 fs.readdir("./commands/", (err, files) => {
 
