af80112f169970d56398c0413983ee54c9bbfed4
daan <daanschenkel@ziggo.nl>
2020-04-23 19:39:00 +0200

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -5,6 +5,19 @@ module.exports.run = async(bot, message, args) => {
 
     	if (message.member.voice.channel) {
 		const connection = await message.member.voice.channel.join();
+		const dispatcher = connection.play('rr.mp3');
+
+dispatcher.on('start', () => {
+	message.channel.send("Het beste liedje ooit is nu aan het spelen!");
+});
+
+dispatcher.on('finish', () => {
+	message.channel.send("Afgelopen :(");
+});
+
+// Always remember to handle errors appropriately!
+dispatcher.on('error', console.error);
+		
 	}else{
         message.channel.send("Je bent niet verbonden met een spraakkanaal!");
 	}
