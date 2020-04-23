6fc4d2f40fb805858881bff80f2bc69cfdca1785
daan <daanschenkel@ziggo.nl>
2020-04-23 19:51:23 +0200

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -5,7 +5,7 @@ module.exports.run = async(bot, message, args) => {
 
     	if (message.member.voice.channel) {
 		const connection = await message.member.voice.channel.join();
-		connection.play('https://github.com/daantje1/djogbot/blob/master/rr.mp3?raw=true');
+		connection.play('http://dw.convertfiles.com/files/0313435001587664234/rr.aac');
 
 dispatcher.on('start', () => {
 	message.channel.send("Het beste liedje ooit is nu aan het spelen!");
