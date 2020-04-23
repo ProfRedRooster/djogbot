b5feee95ac901d0f057a1c81e82e69103008f7af
daan <daanschenkel@ziggo.nl>
2020-04-23 19:32:48 +0200

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -3,10 +3,10 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
 
-       const channel = message.member.voiceChannel; 
-       channel.join().then(connection => console.log('Connected!')).catch(console.error);
-            
-
+    const channel = message.member.voiceChannel;
+    channel.join()
+    .then(connection => message.channel.send('Verbonden'))
+    .catch(console.error);
 
 
 }
