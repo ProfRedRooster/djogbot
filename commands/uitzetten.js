cd13fd31e3d1c9b3415f81bb6bb292ebe75935f8
daan <daanschenkel@ziggo.nl>
2020-05-07 20:01:30 +0200

    Update uitzetten.js

--- commands/uitzetten.js
+++ commands/uitzetten.js
@@ -1,12 +1,7 @@
 const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
- message.channel.send("**Uitgezet voor 5 minuten**");
- bot.destroy();
-  setTimeout(function(){ 
-         bot.login(process.env.token);
-         return message.channel.send("Ben er weer!");
-         }, 300000); //time in milliseconds
+ message.channel.send("**In aanbouw**");
 }
 module.exports.help = {
     name: "zetuit"
