10baf8cb5cb3570c23c2757eda2260c834ff09ee
daan <daanschenkel@ziggo.nl>
2020-05-07 19:44:04 +0200

    Uitzetten gefixt

--- commands/uitzetten.js
+++ commands/uitzetten.js
@@ -4,7 +4,7 @@ module.exports.run = async(bot, message, args) => {
 
  message.delete();
  const member = message.guild.member(message.author);
-if(!message.guild.member(message.author).hasPermission('MANAGE_SERVER')) {
+if(!message.guild.member(message.author).hasPermission('MANAGE_WEBHOOKS')) {
   return message.channel.send(":x: Geen toegang. :x:") 
 }
 message.reply("Bot wordt uitgezet voor **5 minuten**");
@@ -13,6 +13,7 @@ client.destroy()
       client.login(process.env.token)
       return message.reply("Ik ben er weer!");
          }, 3000000); //time in milliseconds
+}
 module.exports.help = {
     name: "zetuit"
 }
