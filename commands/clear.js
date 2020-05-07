6f6bdf2fe40eb80be0ea2d31d009de3c2701b026
daan <daanschenkel@ziggo.nl>
2020-05-07 19:18:04 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-message.reply("Het ping commando is uitgeschakeld wegens dingen");
+message.reply("Het clear commando is uitgeschakeld wegens dingen");
 }
 
 module.exports.help = {
