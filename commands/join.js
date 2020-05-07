78e3d43f4b1c3c02b6a741a935e1cb1107f97d2c
daan <daanschenkel@ziggo.nl>
2020-05-07 18:32:10 +0200

    Poging 2

--- commands/join.js
+++ commands/join.js
@@ -7,11 +7,11 @@ module.exports.run = async(bot, message, args) => {
 		const connection = await message.member.voice.channel.join();
 		connection.play('https://cdn-34.anonfile.com/laN5O6wfo9/41018414-1588869557/rr.wav');
 
-dispatcher.on('start', () => {
+connection.on('start', () => {
 	message.channel.send("Het beste liedje ooit is nu aan het spelen!");
 });
 
-dispatcher.on('finish', () => {
+connection.on('finish', () => {
 	message.channel.send("Afgelopen :(");
 	// Option 1
 connection.disconnect();
@@ -21,7 +21,7 @@ voiceChannel.leave();
 });
 
 // Always remember to handle errors appropriately!
-dispatcher.on('error', console.error);
+connection.on('error', console.error);
 		
 	}else{
         message.channel.send("Je bent niet verbonden met een spraakkanaal!");
