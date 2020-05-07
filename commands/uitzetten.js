a245b4bca29e82cf4bc120f1e66e49388d0bd934
daan <daanschenkel@ziggo.nl>
2020-05-07 19:56:22 +0200

    Update uitzetten.js

--- commands/uitzetten.js
+++ commands/uitzetten.js
@@ -1,20 +1,12 @@
 const discord = require("discord.js");
-
 module.exports.run = async(bot, message, args) => {
 
- message.delete();
- const member = message.guild.member(message.author);
-if(!message.guild.member(message.author).hasPermission('MANAGE_WEBHOOKS')) {
-  message.channel.send(":x: Geen toegang. :x:") 
- return console.log("---IEMAND HEEFT GEPROBEERD DE BOT TE STOPPEN---");
-}
-message.reply("Bot wordt uitgezet voor **5 minuten**");
- console.log("---DE BOT IS UITGEZET VOOR 5 MINUTEN---");
-bot.destroy()
- setTimeout(function(){ 
-      bot.login(process.env.token)
-      return message.reply("Ik ben er weer!");
-         }, 3000000); //time in milliseconds
+ message.channel.send("**Uitgezet voor 5 minuten**");
+ client.destroy();
+  setTimeout(function(){ 
+         client.login(process.env.token);
+         return message.channel.send("Ben er weer!");
+         }, 300000); //time in milliseconds
 }
 module.exports.help = {
     name: "zetuit"
