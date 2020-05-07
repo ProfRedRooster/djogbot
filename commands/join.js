468fd2303b8886c62753dfa24b4d22213bc46bc7
daan <daanschenkel@ziggo.nl>
2020-05-07 18:36:13 +0200

    Dingen waardoor de bot crasht opgelost

--- commands/join.js
+++ commands/join.js
@@ -7,19 +7,6 @@ module.exports.run = async(bot, message, args) => {
 		const connection = await message.member.voice.channel.join();
 		connection.play('https://cdn-34.anonfile.com/laN5O6wfo9/41018414-1588869557/rr.wav');
 
-connection.on('start', () => {
-	message.channel.send("Het beste liedje ooit is nu aan het spelen!");
-});
-
-connection.on('finish', () => {
-	message.channel.send("Afgelopen :(");
-	// Option 1
-connection.disconnect();
-
-// Option 2
-voiceChannel.leave();
-});
-
 // Always remember to handle errors appropriately!
 connection.on('error', console.error);
 		
