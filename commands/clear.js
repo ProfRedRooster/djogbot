101442b68b65e087f77346d340f63a8a835af069
daan <daanschenkel@ziggo.nl>
2020-05-07 19:12:35 +0200

    Clear commando uitgeschakeld wegens dingen

--- commands/clear.js
+++ commands/clear.js
@@ -1,19 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-msg = message;
-const amount = args.join(' '); // Amount of messages which should be deleted
-
-if (!amount) return msg.reply('Hoeveel berichten moet ik verwijderen?'); // Checks if the `amount` parameter is given
-if (isNaN(amount)) return msg.reply('Ben ik nou dom of is dat geen nummer'); // Checks if the `amount` parameter is a number. If not, the command throws an error
-
-if (amount > 100) return msg.reply('Zoveel berichten kan ik niet verwijderen!!'); // Checks if the `amount` integer is bigger than 100
-if (amount < 1) return msg.reply('Minder dan 1 bericht kan ik niet weghalen'); // Checks if the `amount` integer is smaller than 1
-message.delete();
-await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
-    msg.channel.bulkDelete(messages) // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
-    message.reply("Ik heb " + amount + " berichten verwijderd!")
-    });
+message.reply("Het ping commando is uitgeschakeld wegens dingen");
 }
 
 module.exports.help = {
